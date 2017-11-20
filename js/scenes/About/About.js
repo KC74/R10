86418553f680b0984f83c610cf3c5571bc0de192
Kenneth Chow <kennethchow74@gmail.com>
2017-11-20 15:07:57 -0800

    Initial commit.

---
+++ js/scenes/About/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import PropTypes from "prop-types";
+
+const About = () => {
+  return (
+    <View>
+      <Text>This is the about scene!</Text>
+    </View>
+  );
+};
+
+export default About;
